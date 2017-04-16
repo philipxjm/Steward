822341beacca5f5563a00aebdf6c25dad6e011a1
Philip Xu <philipxjm1@gmail.com>
2017-04-16 16:22:23 -0400

    set up maven and gitignore

---
+++ src/main/java/steward/App.java
@@ -0,0 +1,13 @@
+package {project-packaging};
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
