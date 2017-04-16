8cd8aa52745c1c7ce62d43fb231be7f68a86f0e2
Philip Xu <philipxjm1@gmail.com>
2017-04-16 16:30:30 -0400

    set up package structures

--- src/main/java/edu/steward/main/Main.java
+++ src/main/java/edu/steward/main/Main.java
@@ -1,10 +1,10 @@
-package {project-packaging};
+package edu.steward.main;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class Main
 {
     public static void main( String[] args )
     {
