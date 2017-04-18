077515db9f9930ab5c90938184cb0ae365a14198
William Povell <wpovell@cs.brown.edu>
2017-04-18 19:51:35 -0400

    Added rough parser for AlphaVantage data.

--- src/main/java/edu/steward/stock/api/AlphaVantageAPI.java
+++ src/main/java/edu/steward/stock/api/AlphaVantageAPI.java
@@ -7,14 +7,21 @@ import edu.steward.stock.api.StockAPI;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 /**
  * Created by mrobins on 4/17/17.
  */
 public class AlphaVantageAPI implements StockAPI {
-
+	private static final Gson GSON = new Gson();
+	
   @Override
   public List<Price> getStockPrices(String ticker, int startTime, int endTime) {
     return null;
@@ -36,6 +43,34 @@ public class AlphaVantageAPI implements StockAPI {
     String url = constructURL(args);
     System.out.println("url is: " + url);
     return JSONRetriever.getJSON(url, 5000);
+  }
+  
+  public Map<String, Map<String,Double>> parseAlphaVantage(String json) {
+	  JsonObject received = GSON.fromJson(json, JsonObject.class);
+	  JsonObject meta = received.get("Meta Data").getAsJsonObject();
+	  
+	  // Meta
+	  String info = meta.get("1. Information").getAsString();
+	  String sym = meta.get("2. Symbol").getAsString();
+	  String last = meta.get("3. Last Refreshed").getAsString();
+	  String inter = meta.get("4. Interval").getAsString();
+	  String output = meta.get("5. Output Size").getAsString();
+	  String timezone = meta.get("6. Time Zone").getAsString();
+	  
+	  // Timeseries
+	  JsonObject timeseries = received.get("Time Series (15min)").getAsJsonObject();
+	  Map<String, Map<String,Double>> ret = new HashMap<>();
+	  for(Map.Entry<String, JsonElement> e : timeseries.entrySet()) {
+		  Map<String, Double> toAdd = new HashMap<>();
+		  JsonObject obj = (JsonObject) e.getValue();
+		  toAdd.put("open", obj.get("1. open").getAsDouble());
+		  toAdd.put("high", obj.get("2. high").getAsDouble());
+		  toAdd.put("low", obj.get("3. low").getAsDouble());
+		  toAdd.put("close", obj.get("4. close").getAsDouble());
+		  toAdd.put("volume", obj.get("5. volume").getAsDouble());
+		  ret.put(e.getKey(), toAdd);
+	  }
+	  return ret;
   }
 
   private String constructURL(Enum... args) {
