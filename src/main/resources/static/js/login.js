8c5e3723831946712ed2517d97bffbd53dce7f89
Kevin Jin <kjin@Kevins-MacBook-Pro-2.local>
2017-04-23 18:11:56 -0400

    starting dashboard js

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -17,7 +17,7 @@ function onSignIn(googleUser) {
 }	
 
 function onSignOut() {
-	var auth2 = gapi.auth2.getAuthInstance();
+	let auth2 = gapi.auth2.getAuthInstance();
 	auth2.signOut().then(function () {
 		console.log('User signed out.');
 	});
