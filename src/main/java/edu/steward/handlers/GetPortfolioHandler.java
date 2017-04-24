63da2426cc9566a6d83494bb2c2c3c670be43202
William Povell <wpovell@cs.brown.edu>
2017-04-24 16:30:03 -0400

    Forgot to add GetPortfolioHandler.

---
+++ src/main/java/edu/steward/handlers/GetPortfolioHandler.java
@@ -0,0 +1,35 @@
+package edu.steward.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+
+import edu.steward.user.Holding;
+import edu.steward.user.User;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class GetPortfolioHandler implements Route {
+
+  private static final Gson GSON = new Gson();
+
+  @Override
+  public Object handle(Request req, Response res) {
+    QueryParamsMap qm = req.queryMap();
+    String name = qm.value("name");
+    User user = new User(req.session().attribute("id"));
+    List<Holding> stocks = user.getPortfolio(name).getHoldings();
+    List<Map<String, Object>> ret = new ArrayList<>();
+    for (Holding stock : stocks) {
+      ret.add(ImmutableMap.of("ticker", stock.getTicker(), "shares",
+          stock.getShares()));
+    }
+
+    return GSON.toJson(ret);
+  }
+}
--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -8,41 +8,51 @@ function getPortfolios () {
 	});
 }
 
+// Click handler for potfolio
 $('.port').click((e) => {
     $('.port').removeClass("active");
     $(e.target).addClass("active");
     $.post('/getPortfolio', {name: e.target.innerText}, (resJson) => {
         let data = JSON.parse(resJson);
         $('#stocks').empty();
+        // Add stocks
         for (let i = 0; i < data.length; i++) {
             let ticker = data[i]["ticker"];
             let shares = data[i]["shares"];
-            $('#stocks').append('<div class="list-group-item list-group-item-action port <#if port_index == 0>active</#if>">${port.name}</div>')
+            $('#stocks').append(`<div class="list-group-item list-group-item-action stock">${ticker} ${shares}</div>`)
         }
     });
     // TODO: Get stocks & update graph
 });
 
-// TODO initialize graph
+// TODO: initialize graph
 
 // Add portfolio button
 $('#addPort').click((e) => {
-    $('#ports').append('<div class="list-group-item list-group-item-action port"><input id="newPort" type="text"></div>');
-    $('#newPort').keydown((e) => {
-        e.preventDefault();
-        console.log(e);
-        if (e.charCode == 13) { // Enter
-            let name = $(e.target).val();
-            if (name) {
-                // Add new port w name
-            } else {
+    if($('#newPort').length == 0) {
+        $('#ports').append('<div class="list-group-item list-group-item-action port"><input id="newPort" type="text"></div>');
+        let inputDiv = $('#newPort').parent();
+        $('#newPort').keydown((e) => {
+            if (e.keyCode == 13) { // Enter
+                e.preventDefault();
+                let name = $(e.target).val();
+                if (name) {
+                    // if name in list => error
+                    // else create
+                    // TODO: Add new port w name
+
+                } else {
+                    // Remove
+                   inputDiv.remove();
+                }
+            } else if (e.keyCode == 27) { // Escape
+                e.preventDefault();
                 // Remove
+                inputDiv.remove();
             }
-        } else if (e.charCode == 0) { // Escape
-            // Remove
-        }
-    });
-    $('#newPort').focus();
+        });
+        $('#newPort').focus();
+    }
 });
 
 let ctx = $('#gains');
