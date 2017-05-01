9e465d563cc353583cce9f08f7ee7bc8ed594829
Matthew Robins <mrobins@Matthews-MacBook-Pro.local>
2017-05-01 13:35:13 -0400

    schema added so build script works on new db

--- schema/users.sql
+++ schema/users.sql
@@ -1,15 +1,23 @@
-CREATE TABLE `UserPortfolios` (
+BEGIN TRANSACTION;
+CREATE TABLE "UserPortfolios" (
 	`PortfolioId`	TEXT NOT NULL UNIQUE,
 	`Name`	TEXT,
 	`UserId`	TEXT NOT NULL,
 	PRIMARY KEY(`PortfolioId`)
 );
-CREATE TABLE `History` (
-	`portfolio`	TEXT NOT NULL UNIQUE,
-	`stock`	TEXT NOT NULL UNIQUE,
+CREATE TABLE "History" (
+	`portfolio`	TEXT NOT NULL,
+	`stock`	TEXT NOT NULL,
 	`time`	INTEGER NOT NULL,
 	`transaction`	INTEGER NOT NULL,
 	`quantity`	INTEGER NOT NULL CHECK(Quantity >= 0),
 	`price`	REAL NOT NULL,
-	FOREIGN KEY(`portfolio`) REFERENCES `UserPortfolios`(`Portfolio`) ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY(`portfolio`) REFERENCES `UserPortfolios`(`PortfolioId`) ON DELETE CASCADE ON UPDATE CASCADE
 );
+CREATE TABLE "Balances" (
+	`portfolio`	TEXT NOT NULL UNIQUE,
+	`balance`	REAL NOT NULL DEFAULT 1000000,
+	PRIMARY KEY(`portfolio`),
+	FOREIGN KEY(`portfolio`) REFERENCES `UserPortfolios`(`PortfolioId`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+COMMIT;
