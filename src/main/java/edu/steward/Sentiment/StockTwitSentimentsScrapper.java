d867a709144f339d7904889192736030514204f5
William Povell <wpovell@cs.brown.edu>
2017-05-03 23:19:16 -0400

    Started spoofing UA for stocktwit scrape

--- src/main/java/edu/steward/Sentiment/StockTwitSentimentsScrapper.java
+++ src/main/java/edu/steward/Sentiment/StockTwitSentimentsScrapper.java
@@ -1,9 +1,9 @@
 package edu.steward.Sentiment;
 
+import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
-import java.io.IOException;
 
 /**
  * Created by Philip on 5/2/17.
@@ -12,11 +12,12 @@ public class StockTwitSentimentsScrapper {
   public static double getSentiments(String ticker) {
     String url = "https://stocktwits.com/symbol/" + ticker;
     try {
-      Document doc = Jsoup.connect(url).get();
-      double bullish = Double.parseDouble(doc.select("span.bullish")
-              .first()
-              .text()
-              .replace("%", ""));
+      Document doc = Jsoup.connect(url)
+          .userAgent(
+              "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0")
+          .get();
+      double bullish = Double.parseDouble(
+          doc.select("span.bullish").first().text().replace("%", ""));
       return bullish;
     } catch (IOException e) {
       e.printStackTrace();
