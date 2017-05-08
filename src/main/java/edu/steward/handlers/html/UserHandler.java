239943ff32329236f549cd113c1e3d0bcb45edd8
William Povell <wpovell@cs.brown.edu>
2017-05-07 22:01:18 -0400

    Fixed user page

--- src/main/java/edu/steward/handlers/html/UserHandler.java
+++ src/main/java/edu/steward/handlers/html/UserHandler.java
@@ -13,20 +13,23 @@ public class UserHandler implements TemplateViewRoute {
 
   @Override
   public ModelAndView handle(Request req, Response res) throws Exception {
-    String name = req.session().attribute("name");
-    String pic = req.session().attribute("pic");
     String userId = req.params(":id");
     Map<String, String> userInfo = DatabaseApi.getUserInfo(userId);
-    System.out.println(userInfo);
+
     Map<String, String> ret = new HashMap<>();
     ret.putAll(userInfo);
+    ret.put("userName", ret.get("user"));
     ret.put("userPic", ret.get("pic"));
     if (userId.equals("ai")) {
-      ret.put("pic", "/img/ai.png");
-    } else {
-      ret.put("pic", pic);
+      ret.put("userPic", "/img/ai.png");
     }
-    ret.put("title", "User: " + name);
+
+    // Current User
+    ret.put("user", req.session().attribute("user"));
+    ret.put("id", req.session().attribute("id"));
+    ret.put("pic", req.session().attribute("pic"));
+
+    ret.put("title", "User: " + ret.get("user"));
     ret.put("id", userId);
     return new ModelAndView(ret, "account.ftl");
   }
